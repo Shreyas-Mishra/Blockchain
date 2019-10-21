8ff494358d425f8440b983b287a64d8e4f96cdf7
Suryansh1004 <51619358+Suryansh1004@users.noreply.github.com>
2019-10-21 04:02:39 -0700

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -1,4 +1,4 @@
-/////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////////////////////////////////////////////////////
 
 /////////////////////// Do not modify the below code ////////////////////////
 
@@ -90,48 +90,49 @@
   
     const myQuestions = [
       {
-        question: "Why blockchain is trustworthy?",  ///// Write the question inside double quotes
-        },
-      {
-        question: "What is the principle of blockchain?",  ///// Write the question inside double quotes
-        },                                  ///// To add more questions, copy the section below 
-      {
-        question: "What is difference between public key and private key?",  ///// Write the question inside double quotes
-        },
-      {
-        question: "What are the benefits of blockchain?",  ///// Write the question inside double quotes
-       },
-      {
-        question: "What are the drawbacks of blockchain?",  ///// Write the question inside double quotes
-        },
-  
-  
-  
-      /* To add more MCQ's, copy the below section, starting from open curly braces ( { )
-          till closing curly braces comma ( }, )
-  
-          and paste it below the curly braces comma ( below correct answer }, ) of above 
-          question
-  
-      Copy below section
-  
-      {
-        question: "This is question n?",
+        question: "What is a blockchain?",  ///// Write the question inside double quotes
         answers: {
-          a: "Option 1",
-          b: "Option 2",
-          c: "Option 3",
-          d: "Option 4"
+          a: "A type of cryptocurrency",                  ///// Write the option 1 inside double quotes
+          b: "A distributed ledger on a peer to peer network",                  ///// Write the option 2 inside double quotes
+          c: "An exchange",                  ///// Write the option 3 inside double quotes
+          d: "A centralized ledger"                   ///// Write the option 4 inside double quotes
+        },
+        correctAnswer: "b"                ///// Write the correct option inside double quotes
+      },
+  
+      {
+       question: "Who created Bitcoin?",  ///// Write the question inside double quotes
+        answers: {
+          a: "Samsung ",                  ///// Write the option 1 inside double quotes
+          b: "John Mcafee",                  ///// Write the option 2 inside double quotes
+          c: "Satoshi Nakamoto",                  ///// Write the option 3 inside double quotes
+          d: "None of the above"                   ///// Write the option 4 inside double quotes
+        },
+        correctAnswer: "c"                ///// Write the correct option inside double quotes
+      },                                  ///// To add more questions, copy the section below 
+                                                            ///// this line
+  
+   {
+        question: "What is a miner?",
+        answers: {
+          a: "A type of blockchain",
+          b: "A person doing calculations to verify a transaction",
+          c: "Computers that validate and process blockchain transactions",
+          d: "An algorithm that predicts the next part of the chain"
+        },
+        correctAnswer: "c"
+      }, 
+      
+  {
+        question: "What is a genesis block?",
+        answers: {
+          a: "A famous block that hardcoded a hash of the Book of Genesis onto the blockchain",
+          b: "The first block after each block halving",
+          c: "The first block of a Blockchain",
+          d: "The 2nd transaction of a Blockchain"
         },
         correctAnswer: "c"
       },
-  
-      Copy above section
-  
-      */
-  
-  
-  
   
     ];
   
